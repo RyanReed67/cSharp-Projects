1f57cb31567a5dbb35a37615451b2d497d92904f
Ryan Reed <ryno5454@gmail.com>
2026-01-28 16:40:02 -0600

    Add files via upload

---
+++ 2b-Assignment_ConsoleWriteLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
